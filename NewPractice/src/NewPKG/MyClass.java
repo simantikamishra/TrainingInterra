b1d5dbafd21b17789e599e61713b86cf43e132da
Simantika <xyz@gmail.com>
2022-12-12 00:19:03 +0530

    Commit from Master Branch

---
+++ NewPractice/src/NewPKG/MyClass.java
@@ -0,0 +1,17 @@
+package NewPKG;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\simantikam\\eclipse-workspace\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.myntra.com");
+		
+
+	}
+
+}
